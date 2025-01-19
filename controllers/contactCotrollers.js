b7558257a39639cb35106801813989270c27115b
Anuj Pandey <anujpandey1701@yahoo.com>
2025-01-19 20:35:52 +0530

    Error Handling Middleware

--- controllers/contactCotrollers.js
+++ controllers/contactCotrollers.js
@@ -1,21 +1,29 @@
-const getContacts = (req,res)=>{
+const asyncHandler = require("express-async-handler");
+
+const getContacts = asyncHandler (async(req,res)=>{
     res.status(200).json({message: "Get all contacts"});
-};
+});
 
-const getContact = (req,res)=>{
+const getContact = asyncHandler (async(req,res)=>{
     res.status(200).json({message: `Get contact for ${req.params.id}`});
-};
+});
 
-const createContact = (req,res)=>{
-    res.status(201).json({message: "Create Contacts"});
-};
+const createContact = asyncHandler (async(req,res)=>{
+    const {name,email,phone} = req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All Fields are mandatory");
+    }
+    res.status(201).json({message: "Create Contact"});
+    console.log(req.body);
+});
 
-const updateContact = (req,res)=>{
+const updateContact = asyncHandler (async(req,res)=>{
     res.status(200).json({message: `Update contact for ${req.params.id}`});
-};
+});
 
-const deleteContact = (req,res)=>{
+const deleteContact = asyncHandler (async(req,res)=>{
     res.status(200).json({message: `Delete contact for ${req.params.id}`});
-};
+});
 
 module.exports = {getContact,getContacts,createContact,updateContact,deleteContact}
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const errHandler = require("./middleware/errorHandler");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 app.use("/api/contacts",require("./routes/contactRoute"));
+app.use(errHandler);
 
 app.listen(port,()=>{
     console.log(`Server running on ${port}`);
