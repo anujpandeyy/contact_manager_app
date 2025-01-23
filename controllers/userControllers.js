a74e686ff9fb61f115bde60fefb7dbb83ae755f2
Anuj Pandey <anujpandey1701@yahoo.com>
2025-01-23 23:39:14 +0530

    User Registration & Password Hashing

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,12 +1,41 @@
-const getCurrentUser = ((req,res)=>{
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require('../models/userModel');
+
+const getCurrentUser = asyncHandler(async(req,res)=>{
     res.json({message:"Current user Information"});
 });
 
-const postLoginUser = ((req,res)=>{
+const postLoginUser = asyncHandler(async(req,res)=>{
     res.json({message:"Login User"});
 });
 
-const postRegisterUser = ((req,res)=>{
+const postRegisterUser = asyncHandler(async(req,res)=>{
+    const {username,email,password} = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error ("All Fields are reuired")
+    }
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error ("User already registered!")
+    }
+    const hashPassword = await bcrypt.hash(password,10);
+    console.log("Hashed Password",hashPassword);
+    const user = await User.create({
+        username,
+        email,
+        password: hashPassword,
+    });
+    console.log(`User created ${user}`);
+    if(user){
+        res.status(201).json({_id:user.id,email: user.email});
+    }
+    else{
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
     res.json({message:"Register the user"});
 });
 
